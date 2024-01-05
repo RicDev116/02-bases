62871a43c432844d5833046d4e9a3c89167425e9
RicDev116 <ricardo_29leyva@outlook.com>
2024-01-05 02:27:23 -0600

    Funcionalidad del contador

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,21 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
+
 export class AppComponent {
+
   public title: string = 'Hola mundo';
-  public counter:number = 10;
 
-  increaseBy(value:number):void{
-    this.counter +=value;
-  }
-
-  decreaseBy(value:number): void {
-    this.counter -=value;
-  }
-
-  reset(): void {
-    this.counter = 10;
-  }
 }
