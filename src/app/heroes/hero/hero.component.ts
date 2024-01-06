36974c32a1102c0c3570093631f4de76406da680
RicDev116 <ricardo_29leyva@outlook.com>
2024-01-06 00:31:13 -0600

    Seccion 4: componente hero y list  hero

---
+++ src/app/heroes/hero/hero.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-hero',
+  templateUrl: './hero.component.html',
+  styleUrl: './hero.component.css'
+})
+export class HeroComponent {
+  public name: string = 'Iron man';
+  public age: number = 45;
+
+  get capitalizedName():string{
+    return this.name.toUpperCase();
+  }
+
+  getHeroDescription(): string{
+    return `${this.name} - ${this.age}`;
+  }
+
+  changeHero(name: string = "Without name"):void {
+    this.name = name;
+  }
+
+  changeAge(age: number){
+    this.age = age;
+  }
+}
